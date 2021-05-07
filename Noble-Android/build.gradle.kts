94451440d935eee6095f025ade8a26d41ed04f70
Fallen <>
2021-05-06 20:12:30 -0500

    Update gradle files

--- Noble-Android/build.gradle.kts
+++ Noble-Android/build.gradle.kts
@@ -9,27 +9,27 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
-    implementation("org.koin:koin-core:2.2.2")
-    implementation("org.koin:koin-androidx-scope:2.2.2")
-    implementation("org.koin:koin-androidx-viewmodel:2.2.2")
-    implementation("org.koin:koin-androidx-fragment:2.2.2")
+    implementation("io.insert-koin:koin-core:3.0.1")
+    implementation("io.insert-koin:koin-androidx-scope:3.0.1")
+    implementation("io.insert-koin:koin-androidx-viewmodel:3.0.1")
+    implementation("io.insert-koin:koin-androidx-fragment:3.0.1")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.10.0")
-    testImplementation("org.koin:koin-test:2.2.2")
+    testImplementation("io.insert-koin:koin-test:3.0.1")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "com.fallencosplay.noble_mp.Noble"
         minSdkVersion(24)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
     }
